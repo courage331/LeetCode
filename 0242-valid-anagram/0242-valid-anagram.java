8f50e37ac9c03a86c42bc39b9ff16b02d7ef0e9c
courage331 <45195546+courage331@users.noreply.github.com>
2022-10-25 18:43:52 +0900

    Time: 79 ms (5.07%), Space: 51 MB (5.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,9 +4,7 @@ class Solution {
         String [] tArr = t.split("");
         Arrays.sort(sArr);
         Arrays.sort(tArr);
-        //t를 재배열 해서 s만들수 있는가 
         if(tArr.length != sArr.length) return false;
-        //if(String.join("",sArr).contains(String.join("",tArr))) return true;
         for(int i=0; i<sArr.length; i++){
             if(!sArr[i].equals(tArr[i])){
                 return false;
